76b6250a16ff1aa9639ace1f6aee7ff289c9fb42
joaoguilhermeS <38133825+joaoguilhermeS@users.noreply.github.com>
2020-07-24 10:30:42 -0300

    Create exemplo_escrita_arquivostxt.cpp

---
+++ exemplo_escrita_arquivostxt.cpp
@@ -0,0 +1,48 @@
+// basic file operations
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Paciente{
+private:
+	string nome;
+	int idade;
+	//Inserir um tipo que veja a hora de acesso
+	
+public:
+	Paciente(){}
+	~Paciente(){}
+	void valorNome(string n){nome=n;}
+	void valorIdade(int i){idade = i;}
+	
+	string valorNome(){return nome;}
+	int valorIdade(){return idade;}
+};
+	
+
+int main () {
+	Paciente p;
+	string nome;
+	int idade;
+	
+	cout<<"\nMe informe o Nome  : ";
+	cin>>nome;
+	
+	cout<<"\nMe informe a idade : ";
+	getchar();
+	cin>>idade;
+	
+	
+	p.valorNome(nome);
+	p.valorIdade(idade);
+
+	ofstream myfile;
+	myfile.open ("example.txt", ios::out | ios::app);
+	myfile << "Paciente : " <<p.valorNome() <<", Idade:"<<p.valorIdade()<<endl;
+
+
+
+	myfile.close();
+	return 0;
+}
